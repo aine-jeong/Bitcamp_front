f8548bb753a6d4e4d48b800d268da9ed59592451
aine_Jeong <mydream9612@naver.com>
2021-04-05 14:32:49 +0900

    21-04-05

--- Hw02_Ain/WebContent/kanban_script.js
+++ Hw02_Ain/WebContent/kanban_script.js
@@ -6,33 +6,34 @@ document.getElementById("done"),
 document.getElementById("trash")]);
 
 removeOnSpill: false.
-on("drag", function (el) {
-  el.className.replace("ex-moved", "");
+//removeOnSpill : true 설정시 drop영역 외에 다른 곳에 drop하면 객체가 사라지도록 변경된다.
+on("drag", function (el) { // drag할 때
+  el.className.replace("ex-moved", ""); // replace
 }).
-on("drop", function (el) {
-  el.className += "ex-moved";
+on("drop", function (el) { // drop할 때
+  el.className += "ex-moved"; // 이동시킨 영역에 추가
 }).
-on("over", function (el, container) {
+on("over", function (el, container) { // drop영역으로 올라왔을 때
   container.className += "ex-over";
 }).
-on("out", function (el, container) {
+on("out", function (el, container) { // 
   container.className.replace("ex-over", "");
 });
 
 /* Vanilla JS to add a new task */
-function addTask() {
+function addTask() { // 할일 추가하기
   /* Get task text from input */
-  var inputTask = document.getElementById("taskText").value;
+  var inputTask = document.getElementById("taskText").value; // 사용자가 입력한 텍스트
   /* Add task to the 'To Do' column */
   document.getElementById("to-do").innerHTML +=
-  "<li class='task'><p>" + inputTask + "</p></li>";
+  "<li class='task'><p>" + inputTask + "</p></li>"; // to-do 영역에, 사용자가 입력한 텍스트를 넣은 할일 li 추가
   /* Clear task text from input after adding task */
-  document.getElementById("taskText").value = "";
+  document.getElementById("taskText").value = ""; //사용자가 텍스트 입력하는 영역 다시 비워주기
 }
 
 /* Vanilla JS to delete tasks in 'Trash' column */
-function emptyTrash() {
+function emptyTrash() { // 할일 비우기
   /* Clear tasks from 'Trash' column */
-  document.getElementById("trash").innerHTML = "";
+  document.getElementById("trash").innerHTML = ""; //trash 영역에 있는 요소들 삭제
 }
 //# sourceURL=pen.js
